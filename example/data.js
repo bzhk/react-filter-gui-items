3a5cc0df12dfd06babf4f3aae6c7ef1ec7026c95
Bartosz <bzhk911@gmail.com>
2020-01-18 00:01:39 +0100

    add example data

---
+++ example/data.js
@@ -0,0 +1,44 @@
+export const data = [
+  {
+    tags: "mountains,winter",
+    link:
+      "https://images.unsplash.com/photo-1578241561880-0a1d5db3cb8a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
+    alt: ""
+  },
+  {
+    tags: "sea,sun",
+    link:
+      "https://images.unsplash.com/photo-1579198203629-e9aed4d27e3b?ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80",
+    alt: ""
+  },
+  {
+    tags: "sea,cliffs",
+    link:
+      "https://images.unsplash.com/photo-1578165219176-ece04edbd053?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
+    alt: ""
+  },
+  {
+    tags: "forest,winter",
+    link:
+      "https://images.unsplash.com/photo-1578167597239-14f8fc680b6b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1556&q=80",
+    alt: ""
+  },
+  {
+    tags: "river,valley",
+    link:
+      "https://images.unsplash.com/photo-1577993625454-1dec02cedd4b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=659&q=80",
+    alt: ""
+  },
+  {
+    tags: "mountains,forest",
+    link:
+      "https://images.unsplash.com/photo-1577998076239-ea6d9a7dcd82?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
+    alt: ""
+  },
+  {
+    tags: "forest,valley",
+    link:
+      "https://images.unsplash.com/photo-1577949239703-4e1d8aa72824?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1492&q=80",
+    alt: ""
+  }
+];
